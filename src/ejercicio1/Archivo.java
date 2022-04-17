faa6546301628755ec31d3e4a127528c0868447f
HernanMVarela <hernan.varela3@alumnos.frgp.utn.edu.ar>
2022-04-17 12:33:31 -0300

    Initial commit

---
+++ src/ejercicio1/Archivo.java
@@ -0,0 +1,70 @@
+package ejercicio1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class Archivo {
+	
+	//Atributos
+	private String ruta;
+	
+	//Getters & setters
+	public String getRuta() {
+		return ruta;
+	}
+
+	public void setRuta(String ruta) {
+		this.ruta = ruta;
+	}
+	
+	//Constructores
+	public Archivo() {
+		
+	}
+	
+	public Archivo(String ruta) {
+		
+		this.ruta = ruta;
+	}
+
+	//Metodos
+	public boolean existe()
+	{
+		File archivo = new File(ruta); 
+		if(archivo.exists())
+		      return true;
+		return false;
+	}
+	
+	public void lee_lineas() {
+		FileReader entrada;
+		try {
+			entrada = new FileReader(ruta);
+			BufferedReader miBuffer = new BufferedReader(entrada);
+			
+		   String linea = "";
+			while (linea != null) {
+				String[] parts = linea.split("-");
+				if(parts[0] != "") {
+				System.out.print("Nombre: " + parts[0] + " ");
+				
+				System.out.print("Apellido: " + parts[1] + " ");
+				System.out.println();
+				}
+				linea = miBuffer.readLine();
+			}
+			miBuffer.close();
+			entrada.close();
+
+		} catch (IOException e) {
+			System.out.println("No se encontro el archivo");
+		}
+	}
+	
+}
